b43c5347d1df8c868a4ad57eebd9fca57b5153f9
jakinpilla <jakinpilla@gmail.com>
2018-08-30 09:03:51 +0900

    modified

---
+++ ISLR_ch03.R
@@ -0,0 +1,28 @@
+# 3. Linear Regression
+## snergy effect(tv ad and radio ad) :: interaction effect
+
+# 3.1 simple linear regresssion
+# Consequently it is a simple matter to compute
+# the probability of observing any value equal to 
+# |t| or larger, asumming bata_1=0. 
+# We call this probability the p_value. Roughly 
+# speaking, we interpret the p_value as follows:
+# a small p-value indicates that it is unlikely
+# to observe such a substantial association 
+# between the predictor and the response due to
+# chance, in the absence of any real association between
+# the predictor and the response.
+
+# 3.1.3 Assessing the accuracy of the model
+# residual standard error(RSE)
+# the R square statistic
+
+# 3.2 multiple linearregression
+
+# 3.2.2. Some Important Question
+# Is There a Relationship between the response and predictors?
+# The large F-statistic suggest that at least one of 
+# the advertising media must be related to sales
+
+
+
