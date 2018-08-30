bbcaf6f8fb91b16d9eed65853e16fec2d28db30f
jakinpilla <jakinpilla@gmail.com>
2018-08-30 17:32:18 +0900

    modified

--- ISLR_ch03.R
+++ ISLR_ch03.R
@@ -163,7 +163,7 @@
 
 # Comparison of Linear Regression with KNN 
 
-## - A small value for K provides the most fkexible fit, which will have low bias but
+## - A small value for K provides the most flexible fit, which will have low bias but
 ## high variance. This variane is due to the fact that the prediction in a given region
 ## is entirely dependent on just one observation
 
@@ -172,6 +172,184 @@
 
 ## As a general rule, parametric method will tend to outperform non-parametric approaches
 ## when there is a small number of observation per predictor
+
+# 3.6 Lab :: Linear Regression
+library(MASS)
+library(ISLR)
+
+fix(Boston)
+names(Boston)
+
+lm.fit = lm(medv ~ lstat, data=Boston)
+lm.fit
+summary(lm.fit)
+
+names(lm.fit)
+coef(lm.fit)
+# in order to obtain a confidence interval for the coefficient estimates :: confint()
+confint(lm.fit)
+predict(lm.fit, data.frame(lstat=(c(5,10,15))), 
+        interval='confidence')
+# fit      lwr      upr
+# 1 29.80359 29.00741 30.59978
+# 2 25.05335 24.47413 25.63256
+# 3 20.30310 19.73159 20.87461 
+attach(Boston)
+plot(lstat, medv)
+abline(lm.fit)
+
+windows()
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+par(mfrow=c(1,1))
+plot(predict(lm.fit), residuals(lm.fit))
+plot(predict(lm.fit), rstudent(lm.fit))
+
+plot(hatvalues(lm.fit))
+which.max(hatvalues(lm.fit))
+
+# 3.6.3 multiple linear regression
+lm.fit = lm(medv ~ lstat + age, data=Boston)
+summary(lm.fit)
+
+lm.fit = lm(medv ~. , data=Boston)
+summary(lm.fit)
+
+library(car)
+vif(lm.fit)
+
+lm.fit1 = lm(medv ~.-age, data=Boston)
+summary(lm.fit1)
+
+# 3.6.4 interaction terms
+
+summary(lm(medv ~ lstat*age, data=Boston))
+
+# 3.6.5 Non-linear Transformation of the predictors
+lm.fit2= lm(medv ~ lstat + I(lstat^2))
+summary(lm.fit2)
+
+# anova() :: this function performs a hypothesis test comparing the two models.
+# The null hypothesis is that the two models fit the data equally well, and
+# and the alternative hypothesis is that the fulll model is superior.
+
+par(mfrow=c(2, 2))
+plot(lm.fit2)
+
+# poly()
+lm.fit5 = lm(medv ~ poly(lstat, 5))
+summary(lm.fit5)
+summary(lm(medv~log(rm), data=Boston))
+
+# 3.6.6 Qualitative Predictors
+fix(Carseats)
+names(Carseats)
+str(Carseats)
+lm.fit = lm(Sales ~.+Income:Advertising + Price:Age, data=Carseats)
+summary(lm.fit)
+attach(Carseats)
+contrasts(ShelveLoc) # return the coding that R uses for the dummy variables
+
+# ShelveLocGood in the regression output is positive indicates that a good shelving 
+# location is associated with high sales
+
+LoadLibraries=function() {
+  library(ISLR)
+  library(MASS)
+  print('The libraries have been loaded')
+}
+
+LoadLibraries
+LoadLibraries()
+
+# 3.7 Exercise
+
+# 1.
+# TV :: p_value = .0001
+# radio : p_value = .0001
+# newspaper : P_value = .8599
+
+# TV and radio are related to sales but no evidence that newspaper is associated with sales
+# in the presence of other predictors
+
+# 2.
+# KNN regression averages the closest observations to estimate prediction, KNN classifier
+# assigns classification group based on majority of closest observations
+
+# 3.
+# Y = 50 + 20*GPA + .07*IQ + 35*Gender + .01*GPA:IQ - 10*GPA:Gender
+# For GPA above 35/10=3.5, males will earn more.
+# (Gender :: 1 for woman, 0 for man)
+
+# 50 + 20*4 + .07*110 + 35*1 + .01*4*110 -10*4*1
+# $ 137100
+
+# False
+# IQ scale is larger than other predictors (~100 versus 1-4 for GPA and 0-1 for gender) so 
+# even if all predictors have the smae impact on salary, coefficients will be smaller for
+# IQ predictors
+
+# 4.
+# (a) Having more predictors generally means better(lower) RSS on training data
+# (b)If the additional predictors lead to overfitting, the testing RSS could be worse(higher)
+# for the cubic regerssion fit
+# (c) The cubic regression fit should produce a better RSS on the training set because
+# it can adjust for the non-linearity
+# (d) Similer to trainig RSS, the cubic regression fit shoud produce a better RSS on the
+# testing set because it can adjust for the non;linearity
+
+# 5.
+
+# 6. 
+
+# 7. 
+
+# 8. 
+data(Auto)
+fit.lm <- lm(mpg ~ horsepower, data=Auto)
+summary(fit.lm)
+
+# 1) yes, there is a relationship between predictor and response
+# 2) p-value is close to 0, relationship is strong
+# 3) Coefficient is negative :  relationship is negative
+# 4) 
+
+new <- data.frame(horsepower=98)
+predict(fit.lm, new)
+predict(fit.lm, new, interval = 'confidence')
+predict(fit.lm, new, interval = 'prediction')
+
+windows()
+plot(Auto$horsepower, Auto$mpg)
+abline(fit.lm, col='red')
+
+par(mfrow=c(2,2))
+plot(fit.lm)
+par(mfrow=c(1,1))
+
+# residuals vs fitted plot shows that the relationship is non-linear
+
+# 9.
+# part a)
+data(Auto)
+pairs(Auto)
+
+# part b)
+str(Auto)
+subset(Auto, select=-name)
+head(subset(Auto, select=-name))
+cor(subset(Auto, select=-name))
+
+# part c)
+fit.lm <- lm(mpg ~.-name, data=Auto)
+summary(fit.lm)
+# - There is a relationship between predictors andn response
+# - weight, year, origin and displacement have statistically significant relationships
+# - .75 coefficient for year suggests that later model year cars have better(higher) mpg
+
+
+
 
 
 
